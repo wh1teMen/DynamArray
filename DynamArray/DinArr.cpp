200cdda427d5958eaafab173e3647bbe9b1a764e
Belo_cg86@itstepru.onmicrosoft.com <Belo_cg86@itstepru.onmicrosoft.com>
2022-06-09 23:53:52 +0500

    добавлен class iterator

--- DynamArray/DinArr.cpp
+++ DynamArray/DinArr.cpp
@@ -1,221 +1,284 @@
 #include<iostream>
 #include"DinArr.h"
 using namespace std;
-MyDinArr::MyDinArr() {};
-MyDinArr::MyDinArr(int size, int* data) {
-	this->size = size;
-	this->capacity = size;
-	this->data = new int[size];
-	for (int i = 0; i < size; i++)
-		this->data[i] = data[i];
-};
-MyDinArr::~MyDinArr() {
-	if (data != nullptr)
-		delete[]data;
-}
-MyDinArr::MyDinArr(const MyDinArr& other) {
+	MyDinArr::MyDinArr() {};
+	MyDinArr::MyDinArr(int size, int* data) {
+		this->size_ = size;
+		this->capacity_ = size;
+		this->data_ = new int[size];
+		for (int i = 0; i < size; i++)
+			this->data_[i] = data[i];
+	};
+	MyDinArr::~MyDinArr() {
+		if (data_ != nullptr)
+			delete[]data_;
+	}
+	MyDinArr::MyDinArr(const MyDinArr& other) {
 	
-	this->size = other.size;
-	this->capacity = size;
-	this->data = new int[other.size]{};
-	for (int i = 0; i < this->size; i++)
-		this->data[i] = other.data[i];
-}
-void MyDinArr::PrintArray()const {
-	for (int i = 0; i < size; i++)
-		cout << data[i]<<" ";
-}
-int& MyDinArr::operator[](int index) {
-	return data[index];
-}
-MyDinArr& MyDinArr::operator= (const MyDinArr& other) {
-	this->size = other.size;
-	if (this->data != nullptr) {
-		delete[]this->data;
+		this->size_ = other.size_;
+		this->capacity_ = size_;
+		this->data_ = new int[other.size_]{};
+		for (int i = 0; i < this->size_; i++)
+			this->data_[i] = other.data_[i];
 	}
-	this->data = new int[other.size]{};
-	for (int i = 0; i < this->size; i++)
-		this->data[i] = other.data[i];
-	return *this;
-}
-bool MyDinArr::operator==(const MyDinArr&& other) {
-	 if (this->size != other.size)return false;
-	 if (this->size == other.size) {
-		 for (int i = 0; i < this->size; i++) {
-			 if (this->data[i] != other.data[i])
-				 return false;
+	void MyDinArr::print_array()const {
+		for (int i = 0; i < size_; i++)
+			cout << data_[i]<<" ";
+	}
+	int& MyDinArr::operator[](int index) {
+		return data_[index];
+	}
+	MyDinArr& MyDinArr::operator= (const MyDinArr& other) {
+		this->size_ = other.size_;
+		if (this->data_ != nullptr) {
+			delete[]this->data_;
+		}
+		this->data_ = new int[other.size_]{};
+		for (int i = 0; i < this->size_; i++)
+			this->data_[i] = other.data_[i];
+		return *this;
+	}
+	bool MyDinArr::operator==(const MyDinArr&& other) {
+		 if (this->size_ != other.size_)return false;
+		 if (this->size_ == other.size_) {
+			 for (int i = 0; i < this->size_; i++) {
+				 if (this->data_[i] != other.data_[i])
+					 return false;
+			 }
+			 return true;
 		 }
-		 return true;
 	 }
- }
-int &MyDinArr::at(int index) {
-	 return data[index];
-}
-int& MyDinArr::front()const {
-	 return data[0];
- }
-int& MyDinArr::back()const {
-	 return data[size - 1];
- }
-int MyDinArr::Capacity()const {
-	 return capacity;
- }
-bool MyDinArr::empty() {
-	 if(capacity > 0) return false;  return true;
- }
-int  MyDinArr::Size() const{
-	 return this->size;
- }
-void MyDinArr::clear() {
-	 delete[]data;
-	 size = 0;
- }
-void MyDinArr::swap(MyDinArr& other,MyDinArr&other2) {
-	 MyDinArr tmp;
-	 tmp = other;
-	 other = other2;
-	 other2 = tmp;
+	int &MyDinArr::at(int index) {
+		 return data_[index];
+	}
+	int& MyDinArr::front()const {
+		 return data_[0];
+	 }
+	int& MyDinArr::back()const {
+		 return data_[size_ - 1];
+	 }
+	int MyDinArr::capacity()const {
+		 return capacity_;
+	 }
+	bool MyDinArr::empty() {
+		 if(capacity_ > 0) return false;  return true;
+	 }
+	int  MyDinArr::size() const{
+		 return this->size_;
+	 }
+	void MyDinArr::clear() {
+		 delete[]data_;
+		 size_ = 0;
+	 }
+	void MyDinArr::swap(MyDinArr& other,MyDinArr&other2) {
+		 MyDinArr tmp;
+		 tmp = other;
+		 other = other2;
+		 other2 = tmp;
 
- }
-void MyDinArr::push_back(int value) {
+	 }
+	void MyDinArr::push_back(int value) {
 
-	int *tmp = new int[size + 1];
+		int *tmp = new int[size_ + 1];
 
-	for (int i = 0; i < size; i++) {
-		tmp[i] = this->data[i];
-	}
-	size += 1;
-	for (int i = 0; i < this->size - 1; i++) {
-		this->data[i] = tmp[i];
-	}
-	data[size - 1] = value;
-	delete[]tmp;
-}
-const int* MyDinArr::Data()const {
-	return data;
-}
-void MyDinArr::reserve(int value) {
-	size = this->size;
-	int* tmp = new int[size];
-	for (int i = 0; i < size; i++) {
-		tmp[i] = data[i];
-	}
-	delete[]data;
-	this->capacity =size+value;
-	this->size = this->capacity;
-	data = new int[capacity] {};
-	for (int i = 0; i < capacity-value; i++) {
-		data[i] = tmp[i];
-	}
-	delete[]tmp;
-}
-void MyDinArr::shrink_to_fit() {
-	int* tmp = new int[size];
-	for (int i = 0; i < size; i++) {
-		tmp[i] = data[i];
-	}
-	capacity = size;
-	delete[]data;
-	data = new int[capacity];
-	for (int i = 0; i < capacity; i++) {
-		data[i] = tmp[i];
-	}
-	delete[]tmp;
-}
-void MyDinArr::emplace_back(int value) {
-	int* tmp = new int[size + 1];
-
-	for (int i = 0; i < size; i++) {
-		tmp[i] = this->data[i];
-	}
-	size += 1;
-	for (int i = 0; i < this->size - 1; i++) {
-		this->data[i] = tmp[i];
-	}
-	data[size - 1] = value;
-	delete[]tmp;
-
-}
-void MyDinArr::pop_back() {
-
-	int* tmp = new int[size];
-	for (int i = 0; i < size; i++) {
-		tmp[i] = data[i];
-	}
-	delete[]data;
-	size -= 1;
-	data = new int[size] {};
-	for (int i = 0; i < size; i++) {
-		data[i] = tmp[i];
-	}
-	delete[]tmp;
-}
-void MyDinArr::insert(int index,int value,int count) {
-		if (capacity < size + count)
-			this->reserve(count);
-		size += count;
-		int* tmp = new int[size];
-		for (int i = 0; i < index; i++){
-			tmp[i] = data[i];
+		for (int i = 0; i < size_; i++) {
+			tmp[i] = this->data_[i];
 		}
-		for (int i = index; i < index + count; i++){
-			tmp[i] = value;
+		size_ += 1;
+		for (int i = 0; i < this->size_ - 1; i++) {
+			this->data_[i] = tmp[i];
 		}
-		for (int i = index + count; i < size; i++){
-			tmp[i] = data[i - count];
+		data_[size_ - 1] = value;
+		delete[]tmp;
+	}
+	const int* MyDinArr::data()const {
+		return data_;
+	}
+	void MyDinArr::reserve(int value) {
+		size_ = this->size_;
+		int* tmp = new int[size_];
+		for (int i = 0; i < size_; i++) {
+			tmp[i] = data_[i];
 		}
-		delete[]data;
-		data = new int[capacity];
-		for (int i = 0; i < size; i++){
-			data[i] = tmp[i];
+		delete[]data_;
+		this->capacity_ =size_+value;
+		this->size_ = this->capacity_;
+		data_ = new int[capacity_] {};
+		for (int i = 0; i < capacity_-value; i++) {
+			data_[i] = tmp[i];
 		}
 		delete[]tmp;
-	
-	
-}
-void MyDinArr::resize(int newsize) {
-	if (newsize > capacity)
-		this->reserve(newsize - capacity);
-	else
-	{
-		int* tmp = new int[size];
-		for (int i = 0; i < size; i++)
-		{
-			tmp[i] = data[i];
-		}
-		delete[]data;
-		capacity = newsize;
-		data = new int[capacity];
-		if (size > capacity)
-			size = capacity;
-		for (int i = 0; i < size; i++)
-		{
-			data[i] = tmp[i];
-		}
 	}
+	void MyDinArr::shrink_to_fit() {
+		int* tmp = new int[size_];
+		for (int i = 0; i < size_; i++) {
+			tmp[i] = data_[i];
+		}
+		capacity_ = size_;
+		delete[]data_;
+		data_ = new int[capacity_];
+		for (int i = 0; i < capacity_; i++) {
+			data_[i] = tmp[i];
+		}
+		delete[]tmp;
+	}
+	void MyDinArr::emplace_back(int value) {
+		int* tmp = new int[size_ + 1];
+
+		for (int i = 0; i < size_; i++) {
+			tmp[i] = this->data_[i];
+		}
+		size_ += 1;
+		for (int i = 0; i < this->size_ - 1; i++) {
+			this->data_[i] = tmp[i];
+		}
+		data_[size_ - 1] = value;
+		delete[]tmp;
+
+	}
+	void MyDinArr::pop_back() {
+
+		int* tmp = new int[size_];
+		for (int i = 0; i < size_; i++) {
+			tmp[i] = data_[i];
+		}
+		delete[]data_;
+		size_ -= 1;
+		data_ = new int[size_] {};
+		for (int i = 0; i < size_; i++) {
+			data_[i] = tmp[i];
+		}
+		delete[]tmp;
+	}
+	void MyDinArr::insert(int index,int value,int count) {
+			if (capacity_ < size_ + count)
+				this->reserve(count);
+			size_ += count;
+			int* tmp = new int[size_];
+			for (int i = 0; i < index; i++){
+				tmp[i] = data_[i];
+			}
+			for (int i = index; i < index + count; i++){
+				tmp[i] = value;
+			}
+			for (int i = index + count; i < size_; i++){
+				tmp[i] = data_[i - count];
+			}
+			delete[]data_;
+			data_ = new int[capacity_];
+			for (int i = 0; i < size_; i++){
+				data_[i] = tmp[i];
+			}
+			delete[]tmp;
+	
+	
+	}
+	void MyDinArr::resize(int newsize) {
+		if (newsize > capacity_)
+			this->reserve(newsize - capacity_);
+		else
+		{
+			int* tmp = new int[size_];
+			for (int i = 0; i < size_; i++)
+			{
+				tmp[i] = data_[i];
+			}
+			delete[]data_;
+			capacity_ = newsize;
+			data_ = new int[capacity_];
+			if (size_ > capacity_)
+				size_ = capacity_;
+			for (int i = 0; i < size_; i++)
+			{
+				data_[i] = tmp[i];
+			}
+		}
 		
-}
-void MyDinArr::erase(int begin, int end) {
-	int count = 0;
-	int* tmp = new int[size];
-	for (int i = 0; i < size; i++){
-		tmp[i] = data[i];
 	}
-	delete[]data;
-	data = new int[size - (end - begin)];
-	for (int i = 0; i < size; i++)
-	{
-		if (i >= begin && i <= end)
-		{
-			count++;
-			continue;
+	void MyDinArr::erase(int begin, int end) {
+		int count = 0;
+		int* tmp = new int[size_];
+		for (int i = 0; i < size_; i++){
+			tmp[i] = data_[i];
 		}
-		data[i - count] = tmp[i];
+		delete[]data_;
+		data_ = new int[size_ - (end - begin)];
+		for (int i = 0; i < size_; i++)
+		{
+			if (i >= begin && i <= end)
+			{
+				count++;
+				continue;
+			}
+			data_[i - count] = tmp[i];
+		}
+		size_-= (end - begin + 1);
+
 	}
-	size-= (end - begin + 1);
-
-}
-
-
-
-
+	int* MyDinArr::iterator::operator->()const {
+		return place_;
+	}
+	int& MyDinArr::iterator::operator*()const {
+		return *place_;
+	};
+	MyDinArr::iterator::operator int* ()const {
+		return place_;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator+(int i)const {
+		   iterator result(*this);
+		   result.place_ += i;
+		   return result;
+	   }
+	MyDinArr::iterator& MyDinArr::iterator:: operator+=(int i) {
+		place_ += i;
+		return *this;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator-(int i)const {
+		iterator result(*this);
+		result.place_ -= i;
+		return result;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator-=(int i) {
+		place_ -= i;
+		return *this;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator--() {
+		place_ -= 1;
+		return *this;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator--(int) {
+		iterator tmp{ *this };
+		place_ -= 1;
+		return tmp;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator++() {
+		place_ += 1;
+		return *this;
+	}
+	MyDinArr::iterator& MyDinArr::iterator:: operator++(int) {
+		iterator tmp{ *this };
+		place_ += 1;
+		return tmp;
+	}
+	MyDinArr::iterator::operator bool()const {
+		return static_cast<bool>(place_);
+	}
+	bool MyDinArr::iterator:: operator==(const iterator&& other)const {
+		return place_ == other.place_;
+	}
+	bool MyDinArr::iterator:: operator<(const iterator&& other)const {
+		return place_ < other.place_;
+	}
+	bool MyDinArr::iterator:: operator<=(const iterator&& other)const {
+		return place_ <= other.place_;
+	}
+	bool MyDinArr::iterator:: operator>(const iterator&& other)const {
+		return place_ > other.place_;
+	}
+	bool MyDinArr::iterator:: operator>=(const iterator&& other)const {
+		return place_ >= other.place_;
+	}
+	int& MyDinArr::iterator:: operator[](int i)const {
+		return place_[i];
+	}
+	
--- DynamArray/DinArr.h
+++ DynamArray/DinArr.h
@@ -8,18 +8,18 @@ MyDinArr(int size, int* data);
 ~MyDinArr();
 MyDinArr(const MyDinArr& other);
 int& operator[](int index);
-void PrintArray()const;
+void print_array()const;
 MyDinArr& operator = (const MyDinArr& other);
 bool operator==(const MyDinArr&& other);
 int& at(int index);
 int& front()const;
 int& back()const;
-int  Capacity()const;
+int  capacity()const;
 bool empty();
-int Size()const;
+int size()const;
 void clear();
 void swap(MyDinArr &other, MyDinArr &other2);
-const int *Data()const;
+const int *data()const;
 void push_back(int value);
 void emplace_back(int index);
 void reserve(int value);
@@ -28,11 +28,43 @@ void pop_back();
 void insert(int index, int value,int count);
 void erase(int begin, int end);
 void resize(int newsize);
-
+class iterator {
+public:
+	int* operator->()const;
+	int& operator*()const;
+	operator int* ()const; 
+	iterator& operator+(int i)const; 
+	iterator& operator+=(int i); 
+	iterator& operator-(int i)const; 
+	iterator& operator-=(int i); 
+	iterator& operator--(); 
+	iterator& operator--(int);
+	iterator& operator++(); 
+	iterator& operator++(int); 
+	operator bool()const; 
+	bool operator==(const iterator&& other)const; 
+	/*bool operator!=(const iterator&& other)const {
+		return !(*this == other);
+	}*/
+	bool operator<(const iterator&& other)const;
+	bool operator<=(const iterator&& other)const; 
+	bool operator>(const iterator&& other)const; 
+	bool operator>=(const iterator&& other)const;
+	int& operator[](int i)const; 
+	const MyDinArr const* from()const {
+		return &collection_;
+	}
 private:
-	int size{0};
-	int capacity{ 0 };
-	int* data=nullptr;
+	iterator() = delete;
+	iterator(const MyDinArr&& position, int* place):collection_(position),place_(place){}
+	friend class MyDinArr;
+	const MyDinArr& collection_;
+	int* place_;
+};
+private:
+	int size_{0};
+	int capacity_{ 0 };
+	int* data_=nullptr;
 };
 
 
